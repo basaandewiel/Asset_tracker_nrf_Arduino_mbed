1ee85885f76873e612da0b76bf31097ad9c88690
Bas aan de Wiel <bas.aan.de.wiel@gmail.com>
2021-07-11 13:42:21 +0100

    first commit; watchdog with time parameter

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include "watchdog.h"
 
-    void Watchdog::init()
+    void Watchdog::init(uint32_t watchdogtimeout)
     {
       //Configure WDT.
       NRF_WDT->CONFIG         = 0x01;     // Configure WDT to run when CPU is asleep
       // 3932159;  // Timeout set to 120 seconds, timeout[s] = (CRV-1)/32768
-      NRF_WDT->CRV            = 999000;    // CRV = timeout * 32768 + 1
+      NRF_WDT->CRV            = watchdogtimeout;    // CRV = timeout * 32768 + 1
       NRF_WDT->RREN           = 0x01;     // Enable the RR[0] reload register
       NRF_WDT->TASKS_START    = 1;        // Start WDT       
     }
